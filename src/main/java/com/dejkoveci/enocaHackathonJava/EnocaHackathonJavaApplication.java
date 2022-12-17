3d68d37b27506da75d3887b5dc1b8859c1a55acf
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-12-17 18:03:11 +0300

    Initial Commit

---
+++ src/main/java/com/dejkoveci/enocaHackathonJava/EnocaHackathonJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dejkoveci.enocaHackathonJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnocaHackathonJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnocaHackathonJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dejkoveci/enocaHackathonJava/EnocaHackathonJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dejkoveci.enocaHackathonJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocaHackathonJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
